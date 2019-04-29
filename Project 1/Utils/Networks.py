f677b32b813dfa87e8da631bcd3854c3b002e347
giutri <giulio.trichilo@gmail.com>
2019-04-29 13:03:58 +0200

    commit

--- Project 1/Utils/Networks.py
+++ Project 1/Utils/Networks.py
@@ -1,3 +1,6 @@
+####################################################################
+# THIS FILE  HAS VARIOUS NETWORKS YOU CAN TRY WITH THE DATA
+####################################################################
 
 import torch
 from torch.autograd import Variable
@@ -5,6 +8,10 @@ from torch import nn
 from torch import optim
 from torch.nn import functional as F
 
+
+#####################################################################
+# THESE TWO NOT USED, BUT IM KEEPING THEM HERE FOR REFERENCE
+#####################################################################
 class MLPNet(nn.Module):
     def __init__(self):
         super(MLPNet, self).__init__()
@@ -37,6 +44,9 @@ class Net(nn.Module):
         x = self.fc2(x)
         return F.softmax(x,1)
 
+########################################################################
+
+# INDEPENDENT CONVOLUTIONAL NETWORK
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
@@ -94,3 +104,81 @@ class CNN(nn.Module):
         x = self.fc2(x)
         #x = nn.LogSoftmax(x)
         return x    # return x for visualization
+
+
+########################################################################
+
+# SHARED CONVOLUTIONAL NETWORK
+class CNN_S(nn.Module):
+    def __init__(self):
+        super(CNN_S, self).__init__()
+        self.conv1 = nn.Sequential(         # input shape (1, 28, 28)
+            nn.Conv2d(
+                in_channels=1,              # input height
+                out_channels=16,            # n_filters
+                kernel_size=2,              # filter size
+                stride=1,                   # filter movement/step
+                padding=4,                  # if want same width and length of this image after Conv2d, padding=(kernel_size-1)/2 if stride=1
+            ),                              # output shape (16, 28, 28)
+            nn.ReLU(),                      # activation
+            #nn.Dropout2d(0.05),
+            nn.MaxPool2d(kernel_size=2),    # choose max value in 2x2 area, output shape (16, 14, 14)
+            nn.Dropout2d(0.01),
+            nn.BatchNorm2d(16),
+        )
+        self.conv2 = nn.Sequential(         # input shape (16, 14, 14)
+            nn.Conv2d(16, 32, 5, 1, 4),     # output shape (32, 14, 14)
+            nn.ReLU(),                      # activation
+            nn.MaxPool2d(2),                # output shape (32, 7, 7)
+            nn.Dropout2d(0.01),
+
+            nn.BatchNorm2d(32),
+
+        )
+
+        self.conv3 = nn.Sequential(         # input shape (16, 14, 14)
+            nn.Conv2d(32, 64, 5, 1, 4),     # output shape (32, 14, 14)
+            nn.ReLU(),                      # activation
+            nn.Dropout2d(0.05),
+
+            nn.MaxPool2d(2),                # output shape (32, 7, 7)
+
+        )
+
+        #self.fc1 = nn.Linear(32 * 7 * 7, 10)   # fully connected layer, output 10 classes
+        self.fc1 = nn.Sequential(
+            nn.Linear(32 * 7 * 7, 20),  # fully connected layer, output 10 classes
+            #nn.BatchNorm1d(32),
+            nn.Dropout(0.01)
+        )
+        self.fc2 = nn.Sequential(
+            nn.Linear(20, 10) ,  # fully connected layer, output 10 classes
+            nn.BatchNorm1d(10),
+            nn.Dropout(0.01)
+        )
+    def forward(self, x):
+        #first convolutional layer
+        _x = torch.reshape(x[:,0,:,:], (-1, 1, 14, 14))
+        _x1 = self.conv1(_x)
+        _x = torch.reshape(x[:,1,:,:], (-1, 1, 14, 14))
+        _x2 = self.conv1(_x)
+
+        #second convolutional layer
+
+        _x1 = self.conv2(_x1)
+        _x2 = self.conv2(_x2)
+
+        #flatten images
+        _x1 = _x1.view(_x1.size(0), -1)           # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
+        _x2 = _x2.view(_x2.size(0), -1)           # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
+
+        #first fc layer
+        _x1 = F.relu(self.fc1(_x1))
+        _x2 = F.relu(self.fc1(_x2))
+
+        #second fc layer
+        _x1 = self.fc2(_x1)
+        _x2 = self.fc2(_x2)
+
+        #x = nn.LogSoftmax(x)
+        return torch.cat((_x1, _x2), 1)   # return x for visualization
--- Project 1/Utils/errs.py
+++ Project 1/Utils/errs.py
@@ -11,3 +11,24 @@ def compute_nb_errors(model, inputt, target, mini_batch_size):
 
     #print(nb_errors)
     return nb_errors, L
+
+def compute_nb_errors_shared(model, inputt, target_0, target_1, mini_batch_size):
+    nb_errors = 0
+    L = []
+    for b in range(0, inputt.size(0), mini_batch_size):
+        output = model(inputt.narrow(0, b, mini_batch_size))
+        out_0 = output[:,0:10]
+        out_1 = output[:,10:20]
+        _, predicted_classes_0 = out_0.data.max(1)
+        _, predicted_classes_1 = out_1.data.max(1)
+
+        L.append([predicted_classes_0, predicted_classes_1])
+        for k in range(mini_batch_size):
+            if target_0.data[b + k, predicted_classes_0[k]] <= 0:
+                nb_errors = nb_errors + 1
+
+            if target_1.data[b + k, predicted_classes_1[k]] <= 0:
+                nb_errors = nb_errors + 1
+
+    #print(nb_errors)
+    return nb_errors, L
