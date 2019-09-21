8c408f61ca41a745653552d89c42bf11637af71b
tomas <girotomas@gmail.com>
2019-09-21 08:19:44 +0200

    commit

--- Project 1/README.md
+++
@@ -1,5 +0,0 @@
-# DeepProj
-Deep Learning Project EPFL 2019
-
-complete one: MNIST_Shared_Pred.ipynb
-The instructions are on the file: ee559-miniprojects.pdf
--- Project 2/2018/Utils/exceptions_check.py
+++
@@ -1,6 +0,0 @@
-from torch import FloatTensor
-
-
-def checkFloatTensor(input):
-    if type(input) is not FloatTensor:
-        raise TypeError('Not Float Tensor')
